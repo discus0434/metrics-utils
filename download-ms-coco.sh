151be7682d7e12ff85da3c7ea33ebfab780ea2a6
discus0434 <discus0434@gmail.com>
2023-12-14 12:10:09 +0900

    add download ms-coco download script

---
+++ download-ms-coco.sh
@@ -0,0 +1,7 @@
+mkdir data
+wget http://images.cocodataset.org/zips/val2014.zip
+wget http://images.cocodataset.org/annotations/annotations_trainval2014.zip
+unzip annotations_trainval2014.zip
+unzip val2014.zip
+rm val2014.zip
+rm annotations_trainval2014.zip
